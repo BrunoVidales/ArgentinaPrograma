90f4ba7acb85e05f8aa2f4f7794e6d5524e87949
BrunoVidales <brunovidales201@gmail.com>
2023-05-20 13:44:34 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,16 @@
+let base;
+let altura;
+let area;
+
+// a) Area del rectangulo base 5 por altura 3.
+base = 5;
+altura = 3;
+area = base * altura;
+console.log(area);
+
+
+// b) Area del rectangulo base 12.5 por altura 5.3.
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log(area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,27 @@
+//4) Escribir un programa JavaScript que calcule el promedio de 3 números.
+let x;
+let y;
+let z;
+let promedio;
+
+
+//4)a.¿Cuál es el promedio entre 5, 7 y 9?
+x = 5;
+y = 7;
+z = 9;
+promedio = (x + y + z) / 3;
+console.log(promedio);
+
+//4)b.¿Cuál es el promedio entre -2 , -3, -8?
+x = -2;
+y = -3;
+z = -8;
+promedio = (x + y + z) / 3;
+console.log(promedio);
+
+//3)c.¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+x = -1.2;
+y = 5.3;
+z = -3.3;
+promedio = (x + y + z) / 3;
+console.log(promedio);
