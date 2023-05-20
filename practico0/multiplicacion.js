c0384236f419a7edf601ef252a67d7f23f535bca
BrunoVidales <brunovidales201@gmail.com>
2023-05-20 13:24:42 -0300

    Programa resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,6 @@
+let num1 = 5;
+let num2 = 4;
+let num3 = 7;
+let multiplicacion = num1 * num2 * num3;
+console.log(multiplicacion); 
+//En la terminal podemos ejecutar diferentes archivos, por ejemplo node resta.js ; node multiplicacion.js
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let num1 = 574;
+let num2 = 500;
+let resta = num1 - num2;
+console.log(resta);
