a4889ecffa2fb1a41a1a6dadc0bda239ad4a7806
BrunoVidales <brunovidales201@gmail.com>
2023-05-23 22:00:20 -0300

    Programa resta y multiplicacion

--- practico0/multiplicacion.js
+++ practico0/multiplicacion.js
@@ -1,6 +1,17 @@
-let num1 = 5;
-let num2 = 4;
-let num3 = 7;
+/*1).b Dado tres números calcule la multiplicación entre ellos y lo imprima por
+pantalla. Deberá guardar este programa en un archivo llamado:
+multiplicacion.js. Ayuda: utilizar console.log.*/
+
+let num1 = 23;
+let num2 = 17;
+let num3 = 61;
 let multiplicacion = num1 * num2 * num3;
-console.log(multiplicacion); 
+console.log('El valor que devuelve mi programa es', multiplicacion); 
+
+//¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+num1 = 2;
+num2 = 17;
+num3 = 6;
+multiplicacion = num1 * num2 * num3;
+console.log('El valor que devuelve mi programa es', multiplicacion); 
 //En la terminal podemos ejecutar diferentes archivos, por ejemplo node resta.js ; node multiplicacion.js
--- practico0/resta.js
+++ practico0/resta.js
@@ -1,4 +1,17 @@
-let num1 = 574;
-let num2 = 500;
-let resta = num1 - num2;
-console.log(resta);
+//1. Escribir un programa JavaScript que:
+
+/*1).a Dado dos números calcule la resta entre ambos; donde el primer número
+es el minuendo y el segundo es el sustraendo. Imprima el resultado por
+pantalla. Deberá guardar este programa en un archivo llamado: resta.js.
+Ayuda: utilizar console.log.*/
+
+let minuendo = 502;
+let sustraendo = 344;
+let resta = minuendo - sustraendo;
+console.log('El minuendo es', minuendo, 'y el sustraendo', sustraendo, 'y el resultado que devuelve es', resta);
+
+//¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+minuendo = 10;
+sustraendo = 2;
+resta = minuendo - sustraendo;
+console.log('El minuendo es', minuendo, 'y el sustraendo', sustraendo, 'y el resultado que devuelve es', resta);
