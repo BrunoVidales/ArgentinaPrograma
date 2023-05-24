aaca2d21fa7f51af02b2c8a12f6046eeba319803
BrunoVidales <brunovidales201@gmail.com>
2023-05-23 22:03:21 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -1,16 +1,22 @@
-let base;
-let altura;
-let area;
+/*Escribir un programa JavaScript que calcule el área de un rectángulo. Deberá
+guardar este programa en un archivo llamado: areaRectangulo.js. Luego,
+responder a las siguientes preguntas utilizando su código para obtener los
+resultados. Ayuda: la ecuación del área de un rectángulo está dada por:*/
+
+let base = 5;
+let altura = 32;
+let area = base * altura;
+console.log("El valor del area del rectángulo de", base, "y de", altura, "es", area);
 
 // a) Area del rectangulo base 5 por altura 3.
 base = 5;
 altura = 3;
 area = base * altura;
-console.log(area);
+console.log("El valor del area del rectángulo de", base, "y de", altura, "es", area);
 
 
 // b) Area del rectangulo base 12.5 por altura 5.3.
 base = 12.5;
 altura = 5.3;
 area = base * altura;
-console.log(area);
+console.log("El valor del area del rectángulo de", base, "y de", altura, "es", area);
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,27 +1,31 @@
-//4) Escribir un programa JavaScript que calcule el promedio de 3 números.
+/*4) Escribir un programa JavaScript que calcule el promedio de 3 números. Deberá
+guardar este programa en un archivo llamado: promedio3.js. Luego, responder
+a las siguientes preguntas utilizando su código para obtener los resultados. La
+ecuación del promedio entre 3 números está dada por:*/
+
 let x;
 let y;
 let z;
 let promedio;
 
 
-//4)a.¿Cuál es el promedio entre 5, 7 y 9?
+//4)a. ¿Cuál es el promedio entre 5, 7 y 9?
 x = 5;
 y = 7;
 z = 9;
 promedio = (x + y + z) / 3;
-console.log(promedio);
+console.log('El promedio de', x, y, z, 'es', promedio);
 
-//4)b.¿Cuál es el promedio entre -2 , -3, -8?
+//4)b. ¿Cuál es el promedio entre -2 , -3, -8?
 x = -2;
 y = -3;
 z = -8;
 promedio = (x + y + z) / 3;
-console.log(promedio);
+console.log('El promedio de', x, y, z, 'es', promedio);
 
-//3)c.¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+//3)c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
 x = -1.2;
 y = 5.3;
 z = -3.3;
 promedio = (x + y + z) / 3;
-console.log(promedio);
+console.log('El promedio de', x, y, z, 'es', promedio);
