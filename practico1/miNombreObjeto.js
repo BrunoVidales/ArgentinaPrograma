5c789ac9aeec395459255e5ab41566789cf6815c
BrunoVidales <brunovidales201@gmail.com>
2023-06-07 01:26:30 -0300

    Agrego programa miNombreObjeto.js

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,11 @@
+/*2. ¿De qué manera representaría las variables del ejercicio anterior en un objeto,
+llamémosle persona?*/ 
+
+
+/*a. Defina una variable de tipo objeto que lleve el nombre persona, y que
+contenga las variables nombre, apellido y edad del ejercicio anterior con
+sus respectivos valores.*/
+let persona = {nombre:'Bruno', apellido:'Vidales', edad:22};
+
+/*b. Imprime por consola un mensaje que respete el siguiente formato:*/
+console.log("Mi objeto persona es el siguiente. " + JSON.stringify(persona));
