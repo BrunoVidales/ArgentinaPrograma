bf34a8ae8549acf2667849a70aed49fc2c0d1c60
BrunoVidales <brunovidales201@gmail.com>
2023-06-07 01:22:54 -0300

    Agrego programa miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,17 @@
+                                //PARTE 1: VARIABLES
+
+//a. Defina una variable nombre.                                
+let nombre;
+//b. Guarde en la variable nombre tu nombre real.
+nombre = 'Bruno';
+
+//c. Defina y asigne, en un mismo paso, la variable apellido. El valor a
+let apellido = 'Vidales';
+
+/*d. Repita el paso anterior, pero esta vez definiendo la variable edad y
+asignandole tu edad real.*/
+let edad = 22;
+
+/*e. Por último, imprime por consola un mensaje que respete el siguiente
+formato:*/
+console.log("Hola, %s %s. Tienes %i años. ", nombre, apellido, edad);
