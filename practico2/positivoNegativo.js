60e61d5d9c4c4b75cc1a18df8654c171d7ea1948
BrunoVidales <brunovidales201@gmail.com>
2023-06-12 22:09:38 -0300

    Agrego programa positivoNegativo.js

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,13 @@
+// EJERCICIOS 1
+
+const readlineSync = require('readline-sync');
+
+let numero = readlineSync.question("Ingrese un numero: ");
+
+if(numero > 0){
+    console.log("El numero es positivo");
+} else if(numero == 0){
+    console.log("El numero es cero");
+} else{
+    console.log("El numero es negativo")
+}
